a1effdf6b2ff64b591727157b8fd26b0d800e68b
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2020-05-08 13:58:44 +0200

    small fix

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// TO DO: check if the only filters are audience filters
+// TO DO: write tests for at least two functions
+// TO DO: refactor to make it more functional & pure
+
+
 //when first entering the site, load the entire list of shows
 let getShows = new Promise(getAllShows); //because this is an asynchronous funtion, we need to wait for it
 getShows.then(function (data) { //when it's done, display the data
@@ -61,7 +66,7 @@ function displayResults(results) {
 
         resultDiv.appendChild(card); //append to the div that holds all cards
 
-        
+
 
     }
 
@@ -83,113 +88,122 @@ function getFilters() {
 
 function applyFilters(filters) {
     let getShows = new Promise(getAllShows); //because this is an asynchronous funtion, we need to wait for it
-    
+
     //first we check if one of the audience filters is checked
     let audienceFilters;
     let familie = false;
     let volwassenen = false;
 
-    for(filter of filters) {
-       if(filter == "familie"){
-           familie = true;
-       } else if (filter == "volwassenen") {
-        volwassenen = true;
-       }
+    for (filter of filters) {
+        if (filter == "familie") {
+            familie = true;
+        } else if (filter == "volwassenen") {
+            volwassenen = true;
+        }
     }
 
     //set the appropriate filters for the showgenreresults function
-    if(familie && volwassenen) { 
+    if (familie && volwassenen) {
         audienceFilters = ["familie", "volwassenen"];
-    
-    } else if (familie) { 
+
+    } else if (familie) {
         audienceFilters = ["familie", ""];
     } else if (volwassenen) {
-       audienceFilters = ["", "volwassenen"];
-    } if (!familie && !volwassenen) {
+        audienceFilters = ["", "volwassenen"];
+    }
+    if (!familie && !volwassenen) {
         audienceFilters = ["familie", "volwassenen"];
     }
     showGenreResults(audienceFilters);
 
     //filter the data based on the filters
-    getShows.then(function (data) { 
-        let results = [];
+    getShows.then(function (data) {
+                let results = [];
 
-        for(item of data.items) { //loop through the items
-            //console.log(item);
+                for (item of data.items) { //loop through the items
+                    //console.log(item);
 
-            if(filters.length > 0){ //if there are filters selected
-                for(filter of filters) { //loop through the filters
+                    if (filters.length > 0) { //if there are filters selected
+                        //if it's not only the audience filters:
+                        if (!(filters.length == 1 && (familie || volwassenen)) && !(filters.length == 2 && (familie && volwassenen))) {
+                                    for (filter of filters) { //loop through the filters
 
-                    if(familie && volwassenen){
-                        if(item["genre-v2"] == filter) { //if the items category or genre corresponds with a selected filter
-                            results.push(item); //add it to the list of results
+                                        if (familie && volwassenen) {
+                                            if (item["genre-v2"] == filter) { //if the items category or genre corresponds with a selected filter
+                                                results.push(item); //add it to the list of results
+                                            }
+                                        } else if (familie) {
+                                            if (item["genre-v2"] == filter && item["category"] == "familie") { //if the items category or genre corresponds with a selected filter
+                                                results.push(item); //add it to the list of results
+                                            }
+                                        } else if (volwassenen) {
+                                            if (item["genre-v2"] == filter && item["category"] == "volwassenen") { //if the items category or genre corresponds with a selected filter
+                                                results.push(item); //add it to the list of results
+                                            }
+                                        } else {
+                                            if (item["genre-v2"] == filter) { //if the items category or genre corresponds with a selected filter
+                                                results.push(item); //add it to the list of results
+                                            }
+                                        }
+
+
+                                    }
+                                } else { //if the only filters are audience filters, just show all of the results
+                                    results = data.items;
+                                }
+
+
+                            }
+                            else { //if there are no filters selected, just show all of the results
+                                results = data.items;
+                            }
+
+
                         }
-                    } else if(familie){
-                        if(item["genre-v2"] == filter && item["category"] == "familie") { //if the items category or genre corresponds with a selected filter
-                            results.push(item); //add it to the list of results
-                        }
-                    } else if(volwassenen){
-                        if(item["genre-v2"] == filter && item["category"] == "volwassenen") { //if the items category or genre corresponds with a selected filter
-                            results.push(item); //add it to the list of results
-                        }
-                    } else {
-                        if(item["genre-v2"] == filter) { //if the items category or genre corresponds with a selected filter
-                            results.push(item); //add it to the list of results
+
+                        //console.log(results);
+                        //display the results on the page
+                        displayResults(results);
+
+                    })
+            }
+
+            showGenreResults(["familie", "volwassenen"]);
+
+
+            //this function calculates how many results there are per genre
+            function showGenreResults(audienceFilters) {
+                let resultList = {
+                    circus: 0,
+                    comedy: 0,
+                    concert: 0,
+                    dans: 0,
+                    figurentheater: 0,
+                    literatuur: 0,
+                    multidisciplinair: 0,
+                    muziektheater: 0,
+                    opera: 0,
+                    theater: 0,
+                };
+
+                let getShows = new Promise(getAllShows); //because this is an asynchronous funtion, we need to wait for it
+                getShows.then(function (data) { //when it's done, we count the results per genre
+                    for (result of data.items) {
+
+                        if (result.category == audienceFilters[0] || result.category == audienceFilters[1]) {
+                            resultList[result["genre-v2"]] += 1; //add one to the count in the resultlist of the genre that the item is
                         }
                     }
-    
-                    
-                }
-            } else { //if there are no filters selected, just show all of the results
-                results = data.items;
+
+
+                    //console.log(resultList);
+                    for (genre in resultList) {
+                        //display the amount of results between brackets next to the filters
+                        document.getElementsByClassName(genre)[0].textContent = genre + ' (' + resultList[genre] + ')';
+                    }
+
+
+
+                })
+
             }
-            
-
-        }
-
-        console.log(results);
-        //display the results on the page
-        displayResults(results); 
-
-    })
-}
-
-showGenreResults(["familie", "volwassenen"]);
-
-
-//this function calculates how many results there are per genre
-function showGenreResults(audienceFilters) {
-    let resultList = {
-        circus: 0,
-        comedy: 0,
-        concert: 0,
-        dans: 0,
-        figurentheater: 0,
-        literatuur: 0,
-        multidisciplinair: 0,
-        muziektheater: 0,
-        opera: 0,
-        theater: 0,
-    };
-
-    let getShows = new Promise(getAllShows); //because this is an asynchronous funtion, we need to wait for it
-    getShows.then(function (data) { //when it's done, we count the results per genre
-        for (result of data.items) {
-
-            if (result.category == audienceFilters[0] || result.category == audienceFilters[1]) {
-                resultList[result["genre-v2"]] += 1; //add one to the count in the resultlist of the genre that the item is
-            }
-        }
-
-
-        //console.log(resultList);
-        for (genre in resultList) {
-            //display the amount of results between brackets next to the filters
-            document.getElementsByClassName(genre)[0].textContent = genre + ' (' + resultList[genre] + ')';
-        }
-
-
-
-    })
-
-}
